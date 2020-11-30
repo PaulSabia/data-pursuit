8c402cf204958c7f113d94414f51d267d50d2d25
Eva <63392496+RexiaEva@users.noreply.github.com>
2020-11-30 12:33:23 +0100

    Update joueur.py
    J'ajoutte déjà ça je ferais les reste cet aprèm

--- python/joueur.py
+++ python/joueur.py
@@ -15,3 +15,25 @@ class Joueur:
     def jeter_des(self):
         des = random.randint(1,6)
         return des
+
+def liste_joueurs():
+    i=1
+    joueur = list()
+    finie = False
+    Connexion.ouvrir_connexion(bdd)
+    bdd.cursor.execute("""UPDATE `joueurs` SET `points_joueur`=0""")
+    while !finie:
+        joueur.append(input('Entrer le nom du joueur ' + i))
+        present = False
+        bdd.cursor.execute("""SELECT `nom_joueur` FROM `joueurs`""")
+        lignes = bdd.cursor.fetchall()
+        for ligne in lignes:
+            if joueur[i-1] == ligne[0]:
+                reference = (joueur[i-1],)
+                present = True
+            else:
+                reference = (joueur[i-1], 0, NULL, 0)
+                bdd.cursor.execute("""INSERT INTO Produits (`nom_joueur`, `points_joueur`, `couleur_joueur`, `record_joueur`) VALUES(%s, %s, %s, %s)""", reference)
+    bdd.link.commit()
+    Connexion.fermer_connexion(bdd)
+    return joueur
